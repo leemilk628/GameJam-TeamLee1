4da82cda8bb55ef2d314f88d3988b01be6169130
Erica <258732169+Erica-a-afk@users.noreply.github.com>
2026-07-24 17:29:42 +0900

    feat/SceneChanger

--- Assets/Eric/01.Scripts/08.Scenes/SceneChanger.cs
+++ Assets/Eric/01.Scripts/08.Scenes/SceneChanger.cs
@@ -1,10 +1,39 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace Eric.Developer._08.Scenes
+namespace Eric.Scenes
 {
-        [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
-        public class SceneChanger : ScriptableObject
+        [Serializable]
+        public enum SceneType
         {
-                
+                MainMenu,
+                Lobby,
+                Game
+        }
+        public class SceneChanger : MonoBehaviour
+        {
+                public static SceneChanger Instance{get; private set;}
+
+                private void OnEnable()
+                {
+                        if (Instance == null)
+                                Instance = this;
+                        
+                        DontDestroyOnLoad(gameObject);
+                }
+
+                public void ChangeSceneState(SceneType sceneType)
+                {
+                        string sceneName = sceneType switch
+                        {
+                                SceneType.MainMenu => "MainMenuScene",
+                                SceneType.Lobby => "LobbyScene",
+                                SceneType.Game => "GameScene",
+                                _ => ""
+                        };
+                        if (sceneName == "") return;
+                        SceneManager.LoadScene(sceneName);
+                }
         }
 }
