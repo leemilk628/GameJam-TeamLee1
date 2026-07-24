6f902b4d5106d95ddaac9e1a9cab55acc21b31af
Erica <258732169+Erica-a-afk@users.noreply.github.com>
2026-07-25 06:50:11 +0900

    fix/GoldModule

--- Assets/Eric/01.Scripts/05.Gold/GoldModule.cs
+++ Assets/Eric/01.Scripts/05.Gold/GoldModule.cs
@@ -9,7 +9,7 @@ namespace Eric.Currency
     public class GoldModule : MonoBehaviour, IModule
     {
         private ModuleOwner Owner { get; set; }
-        private MeteoriteFragmentModule _meteoriteFragmentModule;
+        [SerializeField]private MeteoriteFragmentModule meteoriteFragmentModule;
         private SkillTreeUpgradeModule _skillTreeUpgradeModule;
 
         public int CurrentGold { get; private set; }
